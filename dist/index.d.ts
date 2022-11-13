2310244e73cd9621a471392fe00e7f47f9fcff35
Nikita Babko <niki.babko@gmail.com>
2022-11-13 18:44:35 +0700

    Fixed package.json 'pre:commit' script

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,12 @@
 /**
  * @description uuidv4 method
- * @return {string}
+ * @returns {string} - id string
  */
 export declare const uuidv4: () => string;
 /**
  * @description shortId method
- * @param {number} length - Password length.
- * @return {string}
+ * @param {number} length - password length
+ * @returns {password: string}
  */
 export declare const shortId: (length?: number) => string;
 //# sourceMappingURL=index.d.ts.map
